0885f0834009c7a0d44ba95f4a85ed2b88eda7dc
EGOIST <hi@egoist.dev>
2026-07-18 16:02:00 +0800

    Initial Commit

---
+++ mitty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mitty
+//
+//  Created by egoist on 7/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mitty/mittyApp.swift
@@ -0,0 +1,17 @@
+//
+//  mittyApp.swift
+//  mitty
+//
+//  Created by egoist on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct mittyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
